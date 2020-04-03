43658e5468ac562cd9b15a024dfa03e6bf2eacdb
adamjosephkeen <44175024+adamjosephkeen@users.noreply.github.com>
2020-04-03 19:24:21 -0400

    Add files via upload

--- StageCrew/App.xaml.cs
+++ StageCrew/App.xaml.cs
@@ -11,7 +11,7 @@ namespace StageCrew
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
