224676a3dfe0fde33a12660ab5f2155e0c2b536f
adamjosephkeen <44175024+adamjosephkeen@users.noreply.github.com>
2020-04-03 17:15:26 -0400

    Add files via upload

---
+++ StageCrew/Models/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageCrew.Models
+{
+    class Constants
+    {
+        public static bool isDev = true;
+    }
+}
